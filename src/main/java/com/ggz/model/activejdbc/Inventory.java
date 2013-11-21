d352618f189b2bd6a4c8fb39ae1534036f716bef
NimonikMike <viet_mike.pham@hotmail.com>
2013-11-21 16:45:14 -0500

    ggz, store controller, inventory, webxml

--- src/main/java/com/ggz/model/activejdbc/Inventory.java
+++ src/main/java/com/ggz/model/activejdbc/Inventory.java
@@ -1,11 +1,33 @@
 package com.ggz.model.activejdbc;
 
+import org.javalite.activejdbc.LazyList;
+import org.javalite.activejdbc.Model;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created with IntelliJ IDEA.
- * User: Wais
+ * User: Mike
  * Date: 11/20/13
- * Time: 7:43 PM
+ * Time: 3:12 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Inventory {
+public class Inventory extends Model{
+
+    private Inventory g;
+
+    public Inventory() {}
+
+    public Inventory(Model g)
+    {
+        this.g = (Inventory)g;
+    }
+
+    public LazyList<Game> getItems(){
+        return this.getAll(Game.class);
+    }
 }
