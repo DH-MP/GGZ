e2624352cc70622051295d41410db99b7d85b85c
snwfog <donchoa@gmail.com>
2013-11-26 04:11:17 -0500

    Import a few package that were missing due to the refactor

--- src/main/java/com/ggz/controller/LoginController.java
+++ src/main/java/com/ggz/controller/LoginController.java
@@ -11,7 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LoginController extends HttpServlet{
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
