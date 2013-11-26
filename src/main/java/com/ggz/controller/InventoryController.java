3551c0b88e85a6f8a8865c547de4388e6c37330f
Wais Khedri <khedriwais@hotmail.com>
2013-11-26 01:11:31 -0500

    fixed import path for controller

--- src/main/java/com/ggz/controller/InventoryController.java
+++ src/main/java/com/ggz/controller/InventoryController.java
@@ -1,9 +1,9 @@
 package com.ggz.controller;
 
-import com.ggz.model.activejdbc.Game;
-import com.ggz.model.activejdbc.Inventory;
-import com.ggz.model.activejdbc.Item;
-import org.javalite.activejdbc.Base;
+import com.ggz.model.Game;
+import com.ggz.model.Inventory;
+import com.ggz.model.Item;
+//import org.javalite.activejdbc.Base;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -24,8 +24,8 @@ public class InventoryController extends HttpServlet
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
     {
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/soen387", "root", "root");
-        System.out.println(new Inventory().getItems());
+        //Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost:3306/soen387", "root", "root");
+        //System.out.println(new Inventory().getItems());
     }
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
