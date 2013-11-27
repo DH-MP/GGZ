be5083586155cf22f7ab8436a7eb1f07f4209d52
NimonikMike <viet_mike.pham@hotmail.com>
2013-11-27 18:04:00 -0500

    manage game and console

--- src/main/java/com/ggz/controller/ManageGameController.java
+++ src/main/java/com/ggz/controller/ManageGameController.java
@@ -1,6 +1,7 @@
 package com.ggz.controller;
 
 import com.ggz.model.Game;
+import com.ggz.model.Image;
 import com.ggz.model.ShoppingCart;
 import org.apache.commons.lang3.StringUtils;
 
@@ -26,14 +27,15 @@ public class ManageGameController  extends HttpServlet
 {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Integer gameId = null;
-        if(req.getAttribute("id") == null){
-            //Get
-            gameId = req.getParameter("id") == null ? null : Integer.parseInt(req.getParameter("id"));
-        }else{
-            //Post Redirect
-            gameId =  req.getAttribute("id") == 0 ? null : Integer.parseInt(req.getAttribute("id").toString());
+        if(req.getParameter("delete")!= null){
+            Game g = new Game().find(Integer.parseInt(req.getParameter("delete")));
+            g.destroy();
+            resp.sendRedirect("/MI.do");
+            return;
         }
+
+
+        Integer gameId = req.getParameter("id") == null ? null : Integer.parseInt(req.getParameter("id"));
 
         Game game = null;
         System.out.println(gameId);
@@ -51,8 +53,38 @@ public class ManageGameController  extends HttpServlet
 
         Map<String, String[]> map = new HashMap(req.getParameterMap());
         Integer id = Integer.parseInt(map.remove("id")[0]);
+        String imageTinyURL = map.remove("imageTinyURL")[0];
+        String imageMediumURL = map.remove("imageMediumURL")[0];
+        String imageLargeURL = map.remove("imageLargeURL")[0];
+
         Game g = id == null | id == 0 ? (Game) new Game(): (Game) new Game().find(id);
         Class<?> gameClassObject = g.getClass();
+
+        if(id != 0){
+            if(g.getImage() == null){
+                Image a = new Image();
+                a.setTinyUrl(imageTinyURL);
+                a.setMediumUrl(imageMediumURL);
+                a.setLargeUrl(imageLargeURL);
+                a.setGame(g);
+                a.save();
+                g.setImage(a);
+            } else{
+                Image a = g.getImage();
+                a.setTinyUrl(imageTinyURL);
+                a.setMediumUrl(imageMediumURL);
+                a.setLargeUrl(imageLargeURL);
+                g.getImage().update();
+            }
+        }else{
+            Image a = new Image();
+            a.setTinyUrl(imageTinyURL);
+            a.setMediumUrl(imageMediumURL);
+            a.setLargeUrl(imageLargeURL);
+            a.setGame(g);
+            a.save();
+            g.setImage(a);
+        }
 
         for(String entry: map.keySet()){
             try {
@@ -86,12 +118,20 @@ public class ManageGameController  extends HttpServlet
 
             } catch (NoSuchFieldException e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                resp.sendRedirect("/MI.do");
+                return;
             } catch (NoSuchMethodException e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                resp.sendRedirect("/MI.do");
+                return;
             } catch (InvocationTargetException e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                resp.sendRedirect("/MI.do");
+                return;
             } catch (IllegalAccessException e) {
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                resp.sendRedirect("/MI.do");
+                return;
             }
         }
 
@@ -104,6 +144,6 @@ public class ManageGameController  extends HttpServlet
             }
         }
 
-        doGet(req, resp);
+        resp.sendRedirect("/MI.do");
     }
 }
