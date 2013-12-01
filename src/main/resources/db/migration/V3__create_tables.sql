ad58dae8443c4aafc47f6aa3ef947827c2b9ed8e
Wais Khedri <khedriwais@hotmail.com>
2013-12-01 17:36:27 -0500

    default value added

--- src/main/resources/db/migration/V3__create_tables.sql
+++ src/main/resources/db/migration/V3__create_tables.sql
@@ -1,24 +1,24 @@
 create table inventories(
   id int not null auto_increment,
-  total_item int,
-  inventory_name varchar(256),
-  address varchar(256),
-  etag varchar(256),
-  semaphore int(1),
+  total_item int default 0,
+  inventory_name varchar(256) default '',
+  address varchar(256) default '',
+  etag varchar(256) default '',
+  semaphore int(1) default 0,
   PRIMARY KEY (id)
 );
 
 create table users(
   id int not null auto_increment,
-  first_name varchar(256),
-  last_name varchar(256),
-  user_name varchar(256),
-  password varchar(256),
-  address varchar(256),
+  first_name varchar(256) default '',
+  last_name varchar(256) default '',
+  user_name varchar(256) default '',
+  password varchar(256) default '',
+  address varchar(256) default '',
   inventory_id int,
   shopping_cart_id int,
-  etag varchar(256),
-  semaphore int(1),
+  etag varchar(256) default '',
+  semaphore int(1) default 0,
   PRIMARY KEY (id),
   FOREIGN KEY(inventory_id) REFERENCES inventories(id)
 );
@@ -26,8 +26,8 @@ create table users(
 create table managers(
   id int not null,
   inventory_id int,
-  etag varchar(256),
-  semaphore int(1),
+  etag varchar(256) default '',
+  semaphore int(1) default 0,
   PRIMARY KEY (id),
   FOREIGN KEY (id) REFERENCES users(id),
   FOREIGN KEY(inventory_id) REFERENCES inventories(id)
@@ -35,11 +35,11 @@ create table managers(
 
 create table shopping_carts(
   id int not null auto_increment,
-  total_price float,
+  total_price float default 0.0,
   user_id int,
-  quantity int,
-  etag varchar(256),
-  semaphore int(1),
+  quantity int default 0,
+  etag varchar(256) default '',
+  semaphore int(1) default 0,
   PRIMARY KEY (id),
   FOREIGN KEY(user_id) REFERENCES users(id)
 );
@@ -79,11 +79,11 @@ create table users_items(
 
 create table orders(
   id int not null auto_increment,
-  status varchar(256),
+  status varchar(256) default '',
   order_date timestamp default '0000-00-00 00:00:00',
   shopping_cart_id int,
   user_id int,
-  etag varchar(256),
+  etag varchar(256) default '',
   semaphore int(1) default 0,
   PRIMARY KEY (id),
   FOREIGN KEY(shopping_cart_id) REFERENCES shopping_carts(id),
@@ -98,20 +98,20 @@ create table managers_orders(
 
 create table credit_cards(
   id int not null auto_increment,
-  cc_number int,
+  cc_number varchar(20) default '',
   user_id int,
   PRIMARY KEY (id),
-  etag varchar(256),
+  etag varchar(256) default '',
   semaphore int(1) default 0,
   FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 create table payments(
   id int not null auto_increment,
-  amount int,
+  amount int default 0,
   user_id int,
   order_id int,
-  etag varchar(256),
+  etag varchar(256) default '',
   semaphore int(1) default 0,
   PRIMARY KEY (id),
   FOREIGN KEY(user_id) REFERENCES users(id),
@@ -121,10 +121,10 @@ create table payments(
 create table receipts(
   id int not null auto_increment,
   receipt_date timestamp default '0000-00-00 00:00:00',
-  amount int,
+  amount int default 0,
   payment_id int,
   user_id int,
-  etag varchar(256),
+  etag varchar(256) default '',
   semaphore int(1) default 0,
   PRIMARY KEY (id),
   FOREIGN KEY(user_id) REFERENCES users(id)
