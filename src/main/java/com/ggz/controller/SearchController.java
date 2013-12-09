c5e1618937079dd8e7f8ada81b0b876c8b1a6eb7
snwfog <donchoa@gmail.com>
2013-12-09 04:06:05 -0500

    Fix the search option for the games

--- src/main/java/com/ggz/controller/SearchController.java
+++ src/main/java/com/ggz/controller/SearchController.java
@@ -1,79 +1,30 @@
 package com.ggz.controller;
 
 import com.ggz.model.Game;
-import com.ggz.model.User;
-import com.sunnyd.database.Manager;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Wais
- * Date: 11/25/13
- * Time: 9:48 PM
- * To change this template use File | Settings | File Templates.
- */
-public class SearchController  extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+public class SearchController extends HttpServlet
+{
+  protected void doPost(HttpServletRequest request,
+      HttpServletResponse response) throws ServletException, IOException
+  {
+    String name = request.getParameter("name");
+    Map<String, Object> condition = new HashMap<String, Object>();
+    condition.put("name", name);
 
-        String name = request.getParameter("name");
-        List<Game> games = new Game().findAll(null);
-
-        System.out.println("gets here");
-
-        Map<String, Object> map = new HashMap<String, Object>();
-        List<Game> matches = new Game().findAll(map);
-        matches.removeAll(matches);
-        System.out.println("cccccc"+matches);
-        Object[] gamesArray = games.toArray();
-        for (Object gameObject : games)
-        {
-
-            Game a = (Game) gameObject;
-
-            if( a.getName().contains(name))
-            {
-                System.out.println(name);
-                map.put("name", a.getName());
-                System.out.println(a.getName());
-                System.out.println("bbbbbbbbbb"+map);
-                matches.add(a);
-
-           }
-
-            //List<Game> matches = new Game().findAll(map);
-        }
-        System.out.println("zzzzzzzzz"+matches);
-        //List<Game> matches = new Game().findAll(map);
-        if (matches.size() >= 1){
-
-            System.out.println("dffdsf"+map);
-            request.setAttribute("matches", matches);
-
-            System.out.println(matches);
-
-        }
-
-        request.getRequestDispatcher("/view/browse.jsp").forward(request, response);
-    }
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        // forward to registration jsp
-//        ServletContext context = getServletContext();
-//        RequestDispatcher dispatcher = context.getRequestDispatcher("/view/register.jsp");
-//        dispatcher.forward(request, response);
-
-    }
-
-
+    List<Game> games = new Game().findAll(condition);
+    request.setAttribute("queryString", name);
+    request.setAttribute("matches", games);
+    request.getRequestDispatcher("/view/browse.jsp")
+        .forward(request, response);
+  }
 }
 
--- src/main/webapp/view/browse.jsp
+++ src/main/webapp/view/browse.jsp
@@ -1,44 +1,44 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Wais
-  Date: 11/25/13
-  Time: 9:11 PM
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page import="com.ggz.model.Game" %>
-<%@ page import="com.ggz.model.Image" %>
 <%@ page import="java.util.List" %>
-<%@ page import="java.util.ArrayList" %>
-<%@ page import="java.util.Map" %>
 <%@ include file="/view/includes/static/header.jsp" %>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 
-<div class="container content">
-    <div class="row row-offcanvas row-offcanvas-right">
-        <div class="col-xs-12 col-sm-7">
-            <div class="row">
-                <% for (Game g : (List<Game>) request.getAttribute("matches"))
-                {
-                    Game game = g;
-                %>
-                <div class="col-6 col-sm-6 col-lg-4 flippable" data-id="<%= game.getId() %>">
-                    <h2><%= game.getName() %></h2>
-                    <div class="box-art-container">
-                        <img src="<%= game.getImage().getMediumUrl() %>" class="img-rounded box-art"/>
-                    </div>
-                    <div>
-                        <span class="label label-primary">PC</span>
-                        <span class="label label-warning">PlayStation</span>
-                    </div>
-                </div>
-                <% } %>
-            </div>
-        </div>
+<div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <a href="/" class="navbar-brand">Gravity Game Zone</a>
     </div>
+  </div>
 </div>
 
+<div class="container content">
+  <div class="page-header">
+    <h2>
+      Your search for <%= request.getAttribute("queryString") %> successfully
+      yielded <%= ((List<?>)request.getAttribute("matches")).size() %> results.
+    </h2>
+  </div>
+  <div class="col-xs-12 col-sm-7">
+    <div class="row">
+      <% for (Game game : (List<Game>) request.getAttribute("matches")) { %>
+      <div class="col-6 col-sm-6 col-lg-4 flippable"
+           data-id="<%= game.getId() %>">
+        <h2><%= game.getName() %>
+        </h2>
 
+        <div class="box-art-container">
+          <img src="<%= game.getImage().getMediumUrl() %>"
+               class="img-rounded box-art"/>
+        </div>
 
-
+        <div>
+          <span class="label label-primary">PC</span>
+          <span class="label label-warning">PlayStation</span>
+        </div>
+      </div>
+      <% } %>
+    </div>
+  </div>
+</div>
 <%@ include file="/view/includes/static/footer.jsp" %>
